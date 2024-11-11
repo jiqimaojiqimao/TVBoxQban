a75cda9c3eb1e0d53ea429e650f5768147950dfc
jiqimaojiqimao <lilijuan_1980@163.com>
2024-11-11 15:10:19 +0800

    Update P2PClass.java

--- app/src/main/java/com/p2p/P2PClass.java
+++ app/src/main/java/com/p2p/P2PClass.java
@@ -2,6 +2,7 @@ package com.p2p;
 
 import java.io.File;
 import java.util.concurrent.Executors;
+import com.github.tvbox.osc.util.FileUtils;
 
 
 public class P2PClass {
@@ -16,17 +17,16 @@ public class P2PClass {
         final String CardPath;
 
         init(String str) {
-            this.CardPath = str;
+			str = FileUtils.getCachePath();
         }
-
         public void run() {
             P2PClass p2PClass = P2PClass.this;
-            p2PClass.path = this.CardPath + "/jpali";
+            p2PClass.path = str + "/jpali";
             File file = new File(P2PClass.this.path);
             if (!file.exists()) {
                 file.mkdirs();
             }
-            P2PClass.port = P2PClass.this.doxstarthttpd("TEST3E63BAAECDAA79BEAA91853490A69F08".getBytes(), this.CardPath.getBytes());
+            P2PClass.port = P2PClass.this.doxstarthttpd("TEST3E63BAAECDAA79BEAA91853490A69F08".getBytes(), str.getBytes());
         }
     }
 
