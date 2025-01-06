644151cea045213669cc2b4b0c9b10c3a65a5854
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-06 18:57:54 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -325,8 +325,8 @@ public class VodController extends BaseController {
 				String Url = ApiConfig.get().musicwallpaper;
 				Picasso.get()
 				.load(Url)
-//				.placeholder(R.drawable.xumusic)
-				.noPlaceholder()
+//				.placeholder(R.drawable.xumusic)   //xuameng默认的站位图
+				.noPlaceholder()   //不使用站位图，效果不好
 				.resize(3840,2160)
 				.centerCrop()
 				.error(R.drawable.xumusic)
