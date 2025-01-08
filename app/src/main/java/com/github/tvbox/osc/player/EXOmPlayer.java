3e278c3cd8a612efb485244291e814b3228485f3
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-08 15:08:29 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -52,7 +52,7 @@ public class EXOmPlayer extends ExoMediaPlayer {
                             t.renderId = groupArrayIndex;
                             data.addAudio(t);
                         } else if (MimeTypes.isText(format.sampleMimeType)) {
-                            String trackName = format.containerMimeType;
+                            String trackName = format.sampleMimeType;
                             TrackInfoBean t = new TrackInfoBean();
                             t.name = trackName;
                             t.language = "";
