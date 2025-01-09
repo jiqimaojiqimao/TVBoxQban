f9b8fee3644a4667b49844153904062dbd6e5487
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-09 21:52:32 +0800

    Update IjkTrackInfo.java

--- player/src/main/java/tv/danmaku/ijk/media/player/misc/IjkTrackInfo.java
+++ player/src/main/java/tv/danmaku/ijk/media/player/misc/IjkTrackInfo.java
@@ -46,6 +46,20 @@ public class IjkTrackInfo implements ITrackInfo {
         return mStreamMeta.mLanguage;
     }
 
+
+    public String getMCodecName() {
+        if (TextUtils.isEmpty(mStreamMeta.mCodecName))
+            return "未知";
+		String zimuCodecs = mStreamMeta.mCodecName;   //xuameng显示字幕类型
+		String text = "hdmv_pgs_subtitle";  //xuameng过滤字幕类型里application/字符串
+		String textString = "pgs";
+		if(zimuCodecs.contains(text)) {  //xuameng过滤字幕类型里application/字符串
+			zimuCodecs = zimuCodecs.replace(text, textString);  //xuameng过滤字幕类型里application/字符串
+		}
+        return zimuCodecs;
+    }
+	
+
     @Override
     public int getTrackType() {
         return mTrackType;
@@ -83,9 +97,11 @@ public class IjkTrackInfo implements ITrackInfo {
                 out.append(mStreamMeta.getSampleRateInline());
                 break;
             case MEDIA_TRACK_TYPE_TIMEDTEXT:
-                out.append("Language");  //xuameng显示语言
-                out.append(", ");
-                out.append(mStreamMeta.mLanguage);
+  //              out.append(mStreamMeta.mLanguage);  //xuameng显示语言
+  //              out.append(", ");
+				out.append("[");
+				out.append(getMCodecName());
+				out.append("字幕]");
                 break;
             case MEDIA_TRACK_TYPE_SUBTITLE:
                 out.append("SUBTITLE");
