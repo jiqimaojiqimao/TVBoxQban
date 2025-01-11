43fb0419ee475eb0f9cada996710902adaa9d3c4
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-11 19:22:20 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -42,7 +42,37 @@ public class EXOmPlayer extends ExoMediaPlayer {
                     for (int formatIndex = 0; formatIndex < group.length; formatIndex++) {
                         Format format = group.getFormat(formatIndex);
                         if (MimeTypes.isAudio(format.sampleMimeType)) {
-                            String trackName = (data.getAudio().size() + 1) + "：" + trackNameProvider.getTrackName(format) + "[" + (TextUtils.isEmpty(format.codecs)?format.sampleMimeType:format.codecs) + "]";
+							String audioCodecs = format.sampleMimeType;
+							String formatCodecs = format.codecs;
+							if (TextUtils.isEmpty(audioCodecs)){
+								audioCodecs = "";
+							}
+							String text = "audio/";  //xuameng过滤字幕类型里application/字符串
+							String textString = "";
+							if(audioCodecs.contains(text)) {  //xuameng过滤字幕类型里application/字符串
+								audioCodecs = audioCodecs.replace(text, textString);  //xuameng过滤字幕类型里application/字符串
+							}
+							String tex3 = "vnd.";  //xuameng过滤字幕类型里application/字符串
+							String textString3 = "";
+							if(audioCodecs.contains(tex3)) {  //xuameng过滤字幕类型里application/字符串
+								audioCodecs = audioCodecs.replace(tex3, textString3);  //xuameng过滤字幕类型里application/字符串
+							}
+							String tex4 = "true-hd";  //xuameng过滤字幕类型里application/字符串
+							String textString4 = "TrueHD";
+							if(audioCodecs.contains(tex4)) {  //xuameng过滤字幕类型里application/字符串
+								audioCodecs = audioCodecs.replace(tex4, textString4);  //xuameng过滤字幕类型里application/字符串
+							}
+
+							if (TextUtils.isEmpty(formatCodecs)){
+								formatCodecs = "";
+							}
+							String text1 = ".40.2";  //xuameng过滤字幕类型里application/字符串
+							String textString1 = "";
+							if(formatCodecs.contains(text1)) {  //xuameng过滤字幕类型里application/字符串
+								formatCodecs = formatCodecs.replace(text1, textString1);  //xuameng过滤字幕类型里application/字符串
+							}
+
+                            String trackName = (data.getAudio().size() + 1) + "：" + trackNameProvider.getTrackName(format) + "[" + (TextUtils.isEmpty(format.codecs)?audioCodecs:formatCodecs) + "]";
 							TrackInfoBean t = new TrackInfoBean();
                             t.name = trackName;
                             t.language = "";
@@ -68,10 +98,15 @@ public class EXOmPlayer extends ExoMediaPlayer {
 							if(originalString.contains(text1)) {  //xuameng过滤字幕类型里application/字符串
 							originalString = originalString.replace(text1, textString1);  //xuameng过滤字幕类型里application/字符串
 							}
-                            String trackName = (data.getSubtitle().size() + 1) + "：" + trackNameProvider.getTrackName(format) + "，"  + "[" + originalString  + "字幕]";  //xuameng显示字幕类型
+							String text2 = "quicktime-";  //xuameng过滤字幕类型里application/字符串
+							String textString2 = "";
+							if(originalString.contains(text2)) {  //xuameng过滤字幕类型里application/字符串
+							originalString = originalString.replace(text2, textString2);  //xuameng过滤字幕类型里application/字符串
+							}
+                            String trackName = "";  //xuameng显示字幕类型
                             TrackInfoBean t = new TrackInfoBean();
                             t.name = trackName;
-                            t.language = "";
+                            t.language = (data.getSubtitle().size() + 1) + "：" + trackNameProvider.getTrackName(format) + "，"  + "[" + originalString  + "字幕]";  //xuameng显示字幕类型
                             t.trackId = formatIndex;
                             t.selected = !StringUtils.isEmpty(subtitleId) && subtitleId.equals(format.id);
                             t.trackGroupId = groupIndex;
