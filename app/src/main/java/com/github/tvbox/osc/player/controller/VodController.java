3eb4a11443131794f58817d5ccf3c4b8feda7c2f
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-13 21:01:38 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -343,29 +343,32 @@ public class VodController extends BaseController {
 	//			.error(R.drawable.xumusic)
 				.memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE)
 				.networkPolicy(NetworkPolicy.NO_CACHE)
-				.into(MxuamengMusic, new Callback() {
 
-                @Override
-                public void onSuccess() {
-
-
-					        if(countDownTimer != null) {
+					   if(countDownTimer != null) {
             countDownTimer.cancel();
         }
         countDownTimer = new CountDownTimer(2500, 500) { //底部epg隐藏时间设定
             public void onTick(long j) {}
             public void onFinish() {
+								.into(MxuamengMusic, new Callback() {
+
+                @Override
+                public void onSuccess() {
                                    MxuamengMusic.setAlpha(0.3f);
                     MxuamengMusic.animate().setDuration(2500).alpha(1f).start();
-            }
-        };
-		        countDownTimer.start();
                 }
 
                 @Override
                 public void onError(Exception e) {
                 }
             });
+
+            }
+        };
+		        countDownTimer.start();
+
+
+
 				}
 			}
         mHandler.postDelayed(this, 15000);
