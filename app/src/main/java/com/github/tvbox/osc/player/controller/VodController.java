2e7a15c3ef53cfa035bdbbd26428bae1d4bb27b3
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-13 14:40:56 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -328,7 +328,7 @@ public class VodController extends BaseController {
 
 
 	String Url = ApiConfig.get().musicwallpaper;
-				Glide.with(super(context))
+				Glide.with(Context)
 				.load(Url)
 //				.placeholder(R.drawable.xumusic)   //xuameng默认的站位图
 //				.noPlaceholder()   //不使用站位图，效果不好
