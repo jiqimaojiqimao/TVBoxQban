a207d5a0f397aafcc0b3cb18cf19b026f1474102
jiqimaojiqimao <lilijuan_1980@163.com>
2025-01-15 14:38:11 +0800

    Update BaseController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/BaseController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/BaseController.java
@@ -73,8 +73,7 @@ public abstract class BaseController extends BaseVideoController implements Gest
 							music_iv_circle_bg.setVisibility(GONE);
 						}
 						
-						
-LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+						LayoutInflater inflater = LayoutInflater.from(context);
 	otherLayout = inflater.inflate(R.layout.activity_live_play, false);
 	circlebgxu = otherLayout.findViewById(R.id.iv_circle_bg_xu);
 						
