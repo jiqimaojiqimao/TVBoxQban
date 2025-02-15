cfda4b55f142e9ae656f653477eb5346251e16eb
jiqimaojiqimao <lilijuan_1980@163.com>
2025-02-15 14:39:41 +0800

    Update PlayFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/PlayFragment.java
@@ -337,6 +337,12 @@ public class PlayFragment extends BaseLazyFragment {
             @Override
             public void openLocalFileChooserDialog() {
 				HawkConfig.intSubtitle = true;  //xuameng判断进入本地字幕
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+					HawkConfig.intSubtitle = false;  //xuameng判断进入本地字幕
+					}
+				}, 800);
                 new ChooserDialog(getActivity())
                         .withFilter(false, false, "srt", "ass", "scc", "stl", "ttml")
                         .withStartFile("/storage/emulated/0/Download")
@@ -352,12 +358,6 @@ public class PlayFragment extends BaseLazyFragment {
             }
         });
         subtitleDialog.show();
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-			HawkConfig.intSubtitle = false;  //xuameng判断进入本地字幕
-            }
-        }, 1500);
     }
 
     @SuppressLint("UseCompatLoadingForColorStateLists")
