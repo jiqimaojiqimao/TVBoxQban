1995ca8a4715f00329f779e9278c80c328f04841
jiqimaojiqimao <lilijuan_1980@163.com>
2025-02-19 20:25:20 +0800

    Update SurfaceRenderView.java

--- app/src/main/java/com/github/tvbox/osc/player/render/SurfaceRenderView.java
+++ app/src/main/java/com/github/tvbox/osc/player/render/SurfaceRenderView.java
@@ -98,9 +98,6 @@ public class SurfaceRenderView extends SurfaceView implements IRenderView, Surfa
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-                if (mMediaPlayer != null) {
-            mMediaPlayer.setDisplay(null);
-        }
 
     }
 }
