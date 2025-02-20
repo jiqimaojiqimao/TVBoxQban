e252f15829a905a76052227b8d7e00b2ecff2fc6
jiqimaojiqimao <lilijuan_1980@163.com>
2025-02-20 18:19:44 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -764,6 +764,13 @@ public class VodController extends BaseController {
             public void onClick(View view) {
                 myHandle.removeCallbacks(myRunnable);
                 myHandle.postDelayed(myRunnable, myHandleSeconds);
+									HawkConfig.intVod = true;  //xuameng判断进入本地字幕
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+			HawkConfig.intVod = false;  //xuameng判断进入本地字幕
+			}
+		}, 2000);
                 try {
                     int playerType = mPlayerConfig.getInt("pl");
                     ArrayList<Integer> exsitPlayerTypes = PlayerHelper.getExistPlayerTypes();
@@ -818,6 +825,13 @@ public class VodController extends BaseController {
                         @Override
                         public void click(Integer value, int pos) {
                             try {
+																	HawkConfig.intVod = true;  //xuameng判断进入本地字幕
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+			HawkConfig.intVod = false;  //xuameng判断进入本地字幕
+			}
+		}, 2000);
                                 dialog.cancel();
                                 int thisPlayType = players.get(pos);
                                 if (thisPlayType != playerType) {
