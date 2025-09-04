edbd5bffc7497d91761274bf29d95d08b31b9ef4
jiqimaojiqimao <lilijuan_1980@163.com>
2025-09-04 14:25:07 +0800

    Add files via upload

--- app/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
+++ app/src/main/java/com/google/android/exoplayer2/ext/ffmpeg/FfmpegLibrary.java
@@ -16,22 +16,16 @@
 package com.google.android.exoplayer2.ext.ffmpeg;
 
 import androidx.annotation.Nullable;
+
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlayerLibraryInfo;
 import com.google.android.exoplayer2.util.LibraryLoader;
 import com.google.android.exoplayer2.util.Log;
 import com.google.android.exoplayer2.util.MimeTypes;
-import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 /**
  * Configures and queries the underlying native library.
- *
- * @deprecated com.google.android.exoplayer2 is deprecated. Please migrate to androidx.media3 (which
- *     contains the same ExoPlayer code). See <a
- *     href="https://developer.android.com/guide/topics/media/media3/getting-started/migration-guide">the
- *     migration guide</a> for more details, including a script to help with the migration.
  */
-@Deprecated
 public final class FfmpegLibrary {
 
   static {
@@ -41,14 +35,14 @@ public final class FfmpegLibrary {
   private static final String TAG = "FfmpegLibrary";
 
   private static final LibraryLoader LOADER =
-      new LibraryLoader("ffmpegJNI") {
+      new LibraryLoader("ffmpeg_jni", "swresample", "avutil", "avcodec") {
         @Override
         protected void loadLibrary(String name) {
           System.loadLibrary(name);
         }
       };
 
-  private static @MonotonicNonNull String version;
+  private static String version;
   private static int inputBufferPaddingSize = C.LENGTH_UNSET;
 
   private FfmpegLibrary() {}
@@ -64,7 +58,9 @@ public final class FfmpegLibrary {
     LOADER.setLibraries(libraries);
   }
 
-  /** Returns whether the underlying library is available, loading it if necessary. */
+  /**
+   * Returns whether the underlying library is available, loading it if necessary.
+   */
   public static boolean isAvailable() {
     return LOADER.isAvailable();
   }
@@ -154,6 +150,10 @@ public final class FfmpegLibrary {
         return "pcm_mulaw";
       case MimeTypes.AUDIO_ALAW:
         return "pcm_alaw";
+      case MimeTypes.VIDEO_H264:
+        return "h264";
+      case MimeTypes.VIDEO_H265:
+        return "hevc";
       default:
         return null;
     }
