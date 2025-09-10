a84005d55d89fb1f8156e4c7b08ca174d198cb33
jiqimaojiqimao <lilijuan_1980@163.com>
2025-09-10 17:51:59 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -73,17 +73,16 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         if (exoSelect > 0) {
             // 选择器优先
             rendererMode = (exoSelect == 1) 
-                ? NextRenderersFactory.EXTENSION_RENDERER_MODE_OFF    // 硬解
+                ? NextRenderersFactory.EXTENSION_RENDERER_MODE_ON    // 硬解
                 : NextRenderersFactory.EXTENSION_RENDERER_MODE_PREFER; // 软解
         } else {
             // 使用exoDecode配置
             rendererMode = exoDecode 
                 ? NextRenderersFactory.EXTENSION_RENDERER_MODE_PREFER // 软解
-                : NextRenderersFactory.EXTENSION_RENDERER_MODE_OFF;   // 硬解
+                : NextRenderersFactory.EXTENSION_RENDERER_MODE_ON;   // 硬解
         }
-        mRenderersFactory = new NextRenderersFactory(mAppContext)
-            .setExtensionRendererMode(rendererMode);
-
+        mRenderersFactory = new NextRenderersFactory(mAppContext);
+        mRenderersFactory.setExtensionRendererMode(rendererMode);
         mRenderersFactory.setEnableDecoderFallback(true);
 
         // xuameng轨道选择器配置
