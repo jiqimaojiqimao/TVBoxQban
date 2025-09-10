78a03f9086f2354e2d219da92a76922b9c60c005
jiqimaojiqimao <lilijuan_1980@163.com>
2025-09-10 16:38:31 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -21,6 +21,7 @@ import androidx.media3.exoplayer.LoadControl;
 import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
 import androidx.media3.exoplayer.trackselection.TrackSelectionArray;
+import io.github.anilbeesetti.nextlib.media3ext.ffdecoder.NextRenderersFactory;
 import androidx.media3.ui.PlayerView;
 import com.github.tvbox.osc.util.HawkConfig;  //xuameng EXO解码
 import com.orhanobut.hawk.Hawk; //xuameng EXO解码
@@ -44,7 +45,7 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
     private boolean mIsPreparing;
 
     private LoadControl mLoadControl;
-    private DefaultRenderersFactory mRenderersFactory;
+    private NextRenderersFactory mRenderersFactory;
     private DefaultTrackSelector mTrackSelector;
     private static AudioTrackMemory memory;    //xuameng记忆选择音轨
 
@@ -72,16 +73,18 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         if (exoSelect > 0) {
             // 选择器优先
             rendererMode = (exoSelect == 1) 
-                ? DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF    // 硬解
-                : DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER; // 软解
+                ? NextRenderersFactory.EXTENSION_RENDERER_MODE_OFF    // 硬解
+                : NextRenderersFactory.EXTENSION_RENDERER_MODE_PREFER; // 软解
         } else {
             // 使用exoDecode配置
             rendererMode = exoDecode 
-                ? DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER // 软解
-                : DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF;   // 硬解
+                ? NextRenderersFactory.EXTENSION_RENDERER_MODE_PREFER // 软解
+                : NextRenderersFactory.EXTENSION_RENDERER_MODE_OFF;   // 硬解
         }
-        mRenderersFactory = new DefaultRenderersFactory(mAppContext)
+        mRenderersFactory = new NextRenderersFactory(mAppContext)
             .setExtensionRendererMode(rendererMode);
+
+        mRenderersFactory.setEnableDecoderFallback(true);
 
         // xuameng轨道选择器配置
         mTrackSelector = new DefaultTrackSelector(mAppContext);
