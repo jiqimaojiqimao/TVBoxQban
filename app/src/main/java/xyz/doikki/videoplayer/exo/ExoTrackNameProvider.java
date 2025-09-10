d6e269f343399d8fc1767eefa15fe12e9dacde97
jiqimaojiqimao <lilijuan_1980@163.com>
2025-09-10 15:32:05 +0800

    Update ExoTrackNameProvider.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoTrackNameProvider.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoTrackNameProvider.java
@@ -4,11 +4,12 @@ import android.content.res.Resources;
 import android.text.TextUtils;
 import androidx.annotation.Nullable;
 import com.github.tvbox.osc.player.R;
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.Format;
-import com.google.android.exoplayer2.util.Assertions;
-import com.google.android.exoplayer2.util.MimeTypes;
-import com.google.android.exoplayer2.util.Util;
+import androidx.media3.common.C;
+import androidx.media3.common.Format;
+import androidx.media3.common.MimeTypes;
+import androidx.media3.common.util.Assertions;
+import androidx.media3.common.util.UnstableApi;
+import androidx.media3.common.util.Util;
 import java.util.Locale;
 public class ExoTrackNameProvider {
     private final Resources resources;
