ee3ff4603d07a4cd4ae67f5528fc10e56cbb6f4b
jiqimaojiqimao <lilijuan_1980@163.com>
2025-09-16 12:10:32 +0800

    Update AmlogicMediaCodecSelector.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/AmlogicMediaCodecSelector.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/AmlogicMediaCodecSelector.java
@@ -1,32 +1,52 @@
+package xyz.doikki.videoplayer.exo;
 
 import android.content.Context;
+import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
 import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
-import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
-import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.video.DefaultRenderersFactory;
 import java.util.List;
 
-public class AmlogicMediaCodecSelector  extends DefaultRenderersFactory {
+public class AmlogicMediaCodecSelector extends DefaultRenderersFactory {
     public AmlogicMediaCodecSelector(Context context) {
-        super(context, MediaCodecSelector.DEFAULT, false);
+        super(context);
     }
 
     @Override
-    public MediaCodecInfo getDecoderInfo(
-            String mimeType, boolean requiresSecureDecoder, boolean requiresTunnelingDecoder) {
+    public void createInputDecoder(
+            String mimeType,
+            boolean requiresSecureDecoder,
+            boolean requiresTunnelingDecoder,
+            MediaCodecSelector mediaCodecSelector,
+            boolean allowOtherCodecs) {
+        // 获取所有可用的解码器信息
         List<MediaCodecInfo> decoderInfos = MediaCodecUtil.getDecoderInfos(
-                mimeType, 
+                mimeType,
                 requiresSecureDecoder,
-                requiresTunnelingDecoder);
-        
+                requiresTunnelingDecoder
+        );
+
         // 优先选择硬件解码器
+        MediaCodecInfo selectedDecoder = null;
         for (MediaCodecInfo info : decoderInfos) {
             if (info.isHardwareAccelerated()) {
-                return info;
+                selectedDecoder = info;
+                break;
             }
         }
-        
-        // 如果没有硬件解码器则返回null（不自动降级到软件解码）
-        return null;
+
+        // 如果没有找到硬件解码器，则抛出异常
+        if (selectedDecoder == null) {
+            throw new IllegalStateException("No hardware decoder available for " + mimeType);
+        }
+
+        // 调用基类方法创建解码器
+        super.createInputDecoder(
+                mimeType,
+                requiresSecureDecoder,
+                requiresTunnelingDecoder,
+                mediaCodecSelector,
+                allowOtherCodecs
+        );
     }
 }
