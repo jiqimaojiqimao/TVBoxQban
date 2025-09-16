a4b1bed7b9a7276a11b4dd24559bb6e3fb811c7a
jiqimaojiqimao <lilijuan_1980@163.com>
2025-09-16 10:57:17 +0800

    Update AmlogicMediaCodecSelector.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/AmlogicMediaCodecSelector.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/AmlogicMediaCodecSelector.java
@@ -2,6 +2,7 @@ package xyz.doikki.videoplayer.exo;
 
 import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
 import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
+import android.media.MediaCodecInfo as AndroidMediaCodecInfo;
 import android.media.MediaCodecList;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,7 +24,8 @@ public class AmlogicMediaCodecSelector implements MediaCodecSelector {
         List<MediaCodecInfo> infos = new ArrayList<>();
         MediaCodecList codecList = new MediaCodecList(MediaCodecList.ALL_CODECS);
         
-        for (MediaCodecInfo info : codecList.getCodecInfos()) {
+        for (AndroidMediaCodecInfo androidInfo : codecList.getCodecInfos()) {
+            MediaCodecInfo info = convertToExoMediaCodecInfo(androidInfo);
             if (isAmlogicDecoder(info)) {
                 infos.add(0, info);
             } else if (info.isHardwareAccelerated()) {
@@ -37,5 +39,16 @@ public class AmlogicMediaCodecSelector implements MediaCodecSelector {
         String name = info.getName();
         return name.contains(AML_SIGNATURE) || 
                Arrays.asList(AML_DECODERS).contains(name);
+    }
+
+    private MediaCodecInfo convertToExoMediaCodecInfo(AndroidMediaCodecInfo androidInfo) {
+        // 这里需要根据实际ExoPlayer版本实现转换逻辑
+        // 简化处理：假设MediaCodecInfo方法兼容
+        return new MediaCodecInfo(
+            androidInfo.getName(),
+            androidInfo.isEncoder(),
+            androidInfo.isSecure(),
+            androidInfo.isHardwareAccelerated()
+        );
     }
 }
