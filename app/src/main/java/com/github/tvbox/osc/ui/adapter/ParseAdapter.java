9c23f040250d5f05b59d33cb0df0c405c1c25e18
jiqimaojiqimao <lilijuan_1980@163.com>
2025-10-02 12:39:38 +0800

    Update ParseAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/ParseAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/ParseAdapter.java
@@ -21,24 +21,24 @@ public class ParseAdapter extends BaseQuickAdapter<ParseBean, BaseViewHolder> {
         TextView tvParse = helper.getView(R.id.tvParse);
         tvParse.setVisibility(View.VISIBLE);
 
-        tvParse.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (item.isDefault()) {
-                    if (hasFocus) {
-                        tvParse.setTextColor(mContext.getResources().getColor(R.color.color_02F8E1));
-                    } else {
-                        tvParse.setTextColor(Color.WHITE);
-                    }
-                }
-            }
-        });
-
         if (item.isDefault()) {
             tvParse.setTextColor(mContext.getResources().getColor(R.color.color_02F8E1));
         } else {
             tvParse.setTextColor(Color.WHITE);
         }
         tvParse.setText(item.getName());
+
+        tvParse.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (item.isDefault()) {
+                    if (hasFocus) {
+                        tvParse.setTextColor(Color.WHITE);
+                    } else {
+                        tvParse.setTextColor(mContext.getResources().getColor(R.color.color_02F8E1));
+                    }
+                }
+            }
+        });
     }
 }
