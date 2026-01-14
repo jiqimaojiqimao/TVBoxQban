61d671a54c50392d50b359253b00f8bda5e07b25
jiqimaojiqimao <lilijuan_1980@163.com>
2026-01-14 18:47:42 +0800

    Update ExoMediaSourceHelper.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaSourceHelper.java
@@ -118,7 +118,7 @@ public final class ExoMediaSourceHelper {
                 return new HlsMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));
             default:
             case C.TYPE_OTHER:
-                return new ProgressiveMediaSource.Factory(factory).createMediaSource(MediaItem.fromUri(contentUri));
+                return new DefaultMediaSourceFactory(getDataSourceFactory(), getExtractorsFactory()).createMediaSource(getMediaItem(uri, errorCode));
         }
     }
 
