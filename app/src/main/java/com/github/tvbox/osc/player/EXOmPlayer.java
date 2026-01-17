025b5af12db0e2a2a0aa2f08927a99ff75409ab5
jiqimaojiqimao <lilijuan_1980@163.com>
2026-01-17 15:00:40 +0800

    Update EXOmPlayer.java

--- app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
+++ app/src/main/java/com/github/tvbox/osc/player/EXOmPlayer.java
@@ -56,12 +56,12 @@ public class EXOmPlayer extends ExoMediaPlayer {
 							if(audioCodecs.contains(text)) {  //xuameng过滤音轨类型里application/字符串
 								audioCodecs = audioCodecs.replace(text, textString);  //xuameng过滤音轨类型里application/字符串
 							}
-							String tex3 = "vnd.";  //xuameng过滤字幕类型里application/字符串
+							String tex3 = "vnd.";  //xuameng过滤音轨类型里application/字符串
 							String textString3 = "";
 							if(audioCodecs.contains(tex3)) {  //xuameng过滤音轨类型里application/字符串
 								audioCodecs = audioCodecs.replace(tex3, textString3);  //xuameng过滤音轨类型里application/字符串
 							}
-							String tex4 = "true-hd";  //xuameng过滤字幕类型里application/字符串
+							String tex4 = "true-hd";  //xuameng过滤音轨类型里application/字符串
 							String textString4 = "TrueHD";
 							if(audioCodecs.contains(tex4)) {  //xuameng过滤音轨类型里application/字符串
 								audioCodecs = audioCodecs.replace(tex4, textString4);  //xuameng过滤音轨类型里application/字符串
@@ -83,6 +83,11 @@ public class EXOmPlayer extends ExoMediaPlayer {
 							String mp4aString = "";
 							if(formatCodecs.contains(mp4a)) {  //xuameng过滤音轨类型里application/字符串
 								formatCodecs = formatCodecs.replace(mp4a, mp4aString);  //xuameng过滤音轨类型里application/字符串
+							}
+							String dtshd = "dts.hd";  //xuameng过滤音轨类型里application/字符串
+							String dtshdString = "dts";
+							if(formatCodecs.contains(mp4a)) {  //xuameng过滤音轨类型里application/字符串
+								formatCodecs = formatCodecs.replace(dtshd, dtshdString);  //xuameng过滤音轨类型里application/字符串
 							}
 
                             String trackName = (data.getAudio().size() + 1) + "：" + trackNameProvider.getTrackName(format) + "[" + (TextUtils.isEmpty(format.codecs)?audioCodecs:formatCodecs) + "]";
