ac834cf4955121da2188b156984d1c4f04e57130
jiqimaojiqimao <lilijuan_1980@163.com>
2026-01-23 13:09:41 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -105,13 +105,13 @@ private long lastCleanTime = 0;  // 声明变量并初始化为0
 
 DefaultLoadControl mLoadControl = new DefaultLoadControl.Builder()
     .setBufferDurationsMs(
-        5000,    // minBufferMs - 减小最小缓冲时间
-        10000,   // maxBufferMs - 减小最大缓冲时间
+        15000,    // minBufferMs - 减小最小缓冲时间
+        30000,   // maxBufferMs - 减小最大缓冲时间
         2000,    // bufferForPlaybackMs - 减小播放前缓冲时间
         3000     // bufferForPlaybackAfterRebufferMs - 减小重新缓冲后缓冲时间
     )
     .setTargetBufferBytes(30 * 1024 * 1024)  // 设置目标缓冲字节数为30MB
-    .setPrioritizeTimeOverSizeThresholds(true)  // 优先考虑时间阈值
+    .setPrioritizeTimeOverSizeThresholds(false)  // 优先考虑字节数阈值
     .build();
 
 
