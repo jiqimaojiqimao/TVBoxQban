905b6ee87564c49717f481f5b5010cd37dde7eb2
jiqimaojiqimao <lilijuan_1980@163.com>
2026-03-13 15:42:58 +0800

    Update ExoMediaPlayer.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaPlayer.java
@@ -83,6 +83,7 @@ public class ExoMediaPlayer extends AbstractPlayer implements Player.Listener {
         if (exoSelect == 2 || (exoSelect == 0 && exoDecode)) {
         // 软解场景：exoSelect=2 或 exoSelect=0且exoDecode=true
             mRenderersFactory = new NextRenderersFactory(mAppContext)
+			.setAudioPrefer(true)	
             .setEnableDecoderFallback(false)
             .setExtensionRendererMode(NextRenderersFactory.EXTENSION_RENDERER_MODE_PREFER);
         } else {
