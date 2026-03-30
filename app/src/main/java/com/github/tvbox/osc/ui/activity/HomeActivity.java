4f9374dbeb28770f0fd7638c51be35c9fd8c7467
jiqimaojiqimao <lilijuan_1980@163.com>
2026-03-30 21:49:13 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -166,6 +166,9 @@ public class HomeActivity extends BaseActivity {
         sortAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {     //xuameng主页默认焦点
             @Override
             public void onChanged() {
+                if (mGridView == null || !mGridView.isAttachedToWindow()) {
+                    return; // 如果RecyclerView为空或未附加到窗口，直接返回
+                }
                 mGridView.post(() -> {
                     RecyclerView.LayoutManager layoutManager = mGridView.getLayoutManager();
                     if (layoutManager == null) {
@@ -582,21 +585,41 @@ public class HomeActivity extends BaseActivity {
             }
             // 如果 sortFocusView 存在且没有获取焦点，则请求焦点
             if (this.sortFocusView != null && !this.sortFocusView.isFocused()) {
+                if (mGridView == null || !mGridView.isAttachedToWindow()) {
+                    return; // 如果RecyclerView为空或未附加到窗口，直接返回
+                }
                 RecyclerView.LayoutManager layoutManager = mGridView.getLayoutManager();
                 if (layoutManager == null) {
                     return; // xuameng防止空指针
                 }
-                //this.sortFocusView.requestFocus(); //xuameng这段代码手机使用时菜单失去焦点会闪退   
-                this.mGridView.setSelection(PositionXu);   //xuameng处理手机滑动主页菜单失去焦点时按返回键闪退
+                if (currentView != null && PositionXu !=0) {   // xuameng防止空指针
+					//this.sortFocusView.requestFocus(); //xuameng这段代码手机使用时菜单失去焦点会闪退   
+                    this.mGridView.setSelection(PositionXu);   //xuameng处理手机滑动主页菜单失去焦点时按返回键闪退
+                }
+
             }
             // 如果当前不是第一个界面，则将列表设置到第一项
             else if (this.sortFocused != 0) {
+                if (mGridView == null || !mGridView.isAttachedToWindow()) {
+                    return; // 如果RecyclerView为空或未附加到窗口，直接返回
+                }
+                RecyclerView.LayoutManager layoutManager = mGridView.getLayoutManager();
+                if (layoutManager == null) {
+                    return; // xuameng防止空指针
+                }
                 this.mGridView.setSelection(0);
             } else {
                 exit();
             }
         } else if (baseLazyFragment instanceof UserFragment && UserFragment.tvHotList1.canScrollVertically(-1)) {
             // 如果 UserFragment 列表可以向上滚动，则滚动到顶部
+            if (mGridView == null || !mGridView.isAttachedToWindow()) {
+                return; // 如果RecyclerView为空或未附加到窗口，直接返回
+            }
+            RecyclerView.LayoutManager layoutManager = mGridView.getLayoutManager();
+            if (layoutManager == null) {
+                return; // xuameng防止空指针
+            }
             UserFragment.tvHotList1.scrollToPosition(0);
             this.mGridView.setSelection(0);
         } else {
@@ -777,6 +800,7 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
         EventBus.getDefault().unregister(this);
         AppManager.getInstance().appExit(0);
         ControlManager.get().stopServer();
@@ -957,6 +981,9 @@ public class HomeActivity extends BaseActivity {
     }
 
     private void resetAllItemsToDefault() {   //xuameng   重置未选中菜单项为默认值 手机上的BUG
+        if (mGridView == null || !mGridView.isAttachedToWindow()) {
+            return; // 如果RecyclerView为空或未附加到窗口，直接返回
+        }
         RecyclerView.LayoutManager layoutManager = mGridView.getLayoutManager();
         if (layoutManager == null) {
             return; // xuameng防止空指针
@@ -978,6 +1005,9 @@ public class HomeActivity extends BaseActivity {
     }
 
     private void resetAllItemsToDefaultPhone() {  
+        if (mGridView == null || !mGridView.isAttachedToWindow()) {
+            return; // 如果RecyclerView为空或未附加到窗口，直接返回
+        }
         RecyclerView.LayoutManager layoutManager = mGridView.getLayoutManager();
         if (layoutManager == null) {
             return; // xuameng防止空指针
