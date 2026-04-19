053de4ce008dc03dfb219695f966aa2a0898a5db
jiqimaojiqimao <lilijuan_1980@163.com>
2026-04-19 14:04:36 +0800

    Update ScreenUtils.java

--- app/src/main/java/com/github/tvbox/osc/util/ScreenUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/ScreenUtils.java
@@ -9,6 +9,7 @@ import android.content.res.Configuration;
 import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
+import android.os.Build;
 
 public class ScreenUtils {
 
@@ -31,9 +32,14 @@ public class ScreenUtils {
         return telephonyManager.getPhoneType() != TelephonyManager.PHONE_TYPE_NONE;
     }
 
+    // xuameng直接判断是否低于Android 6.0 如是就是TV
+    private static boolean isBelowMarshmallow() {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M;
+    }
+
     public static boolean isTv(Context context) {
         UiModeManager uiModeManager = (UiModeManager) context.getSystemService(UI_MODE_SERVICE);
-        return uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION || (checkScreenLayoutIsTv(context) && !checkIsPhone(context));
+        return uiModeManager.getCurrentModeType() == Configuration.UI_MODE_TYPE_TELEVISION || (checkScreenLayoutIsTv(context) && !checkIsPhone(context) || isBelowMarshmallow());
     }
 
 
