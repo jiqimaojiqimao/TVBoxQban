b78b792179e36d9c39e7f4ab48291f04cb7365b7
jiqimaojiqimao <lilijuan_1980@163.com>
2026-05-28 10:06:07 +0800

    Update Movie.java

--- app/src/main/java/com/github/tvbox/osc/bean/Movie.java
+++ app/src/main/java/com/github/tvbox/osc/bean/Movie.java
@@ -10,9 +10,9 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * @author pj567
- * @date :2020/12/18
- * @description:
+ * @author xuameng
+ * @date :2026/05/28
+ * @description:   接口action方式支持
  */
 @XStreamAlias("list")
 public class Movie implements Serializable {
@@ -64,6 +64,8 @@ public class Movie implements Serializable {
         public String sourceKey;
         @XStreamAlias("tag")
         public String tag;
+        @XStreamAlias("action")
+        public String action; //: xuameng 兼容接口自定义动作
 
         @XStreamAlias("dl")
         public static class UrlBean implements Serializable {
