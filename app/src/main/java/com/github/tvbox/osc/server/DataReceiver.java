bf310fae672bb5de6cb9f0ecefeb32ebd856c87e
jiqimaojiqimao <lilijuan_1980@163.com>
2026-06-27 15:45:48 +0800

    Update DataReceiver.java

--- app/src/main/java/com/github/tvbox/osc/server/DataReceiver.java
+++ app/src/main/java/com/github/tvbox/osc/server/DataReceiver.java
@@ -1,9 +1,9 @@
 package com.github.tvbox.osc.server;
 
 /**
- * @author pj567
- * @date :2021/1/5
- * @description:
+ * @author xuameng
+ * @date :2026/6/24
+ * @description: 直播远程输入
  */
 public interface DataReceiver {
 
@@ -15,7 +15,11 @@ public interface DataReceiver {
 
     void onApiReceived(String url);
 
+    void onLiveApiReceived(String url); //xuameng直播远程输入
+
     void onPushReceived(String url);
+
+    void onDanmuApiReceived(String url);  //xuameng 弹幕远程输入
 
 	void onMirrorReceived(String id, String sourceKey);      //xuameng推送
 }
