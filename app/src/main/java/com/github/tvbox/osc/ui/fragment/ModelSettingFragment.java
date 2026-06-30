09ab09b7b516cd2e9b440e2e0edd41f9ad08c03b
jiqimaojiqimao <lilijuan_1980@163.com>
2026-06-30 22:50:28 +0800

    Update ModelSettingFragment.java

--- app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
+++ app/src/main/java/com/github/tvbox/osc/ui/fragment/ModelSettingFragment.java
@@ -927,21 +927,21 @@ public class ModelSettingFragment extends BaseLazyFragment {
 
     private void onClickClearCache(View v) {
         FastClickCheckUtil.check(v);
-        String cachePath = FileUtils.getCachePath();
-        File cacheDir = new File(cachePath);
+        String cachePath = FileUtils.getCachePath();          //xuameng点击清空缓存
         String cspCachePath = FileUtils.getFilePath()+"/csp/";
+        String jarCachePath = FileUtils.getFilePath()+"/jar/";
         File cspCacheDir = new File(cspCachePath);
-        if (!cacheDir.exists() && !cspCacheDir.exists()) return;
+        File jarCacheDir = new File(jarCachePath);
+        File cacheDir = new File(cachePath);
         new Thread(() -> {
             try {
-                if(cacheDir.exists())FileUtils.cleanDirectory(cacheDir);
-                if(cspCacheDir.exists()){
-                    FileUtils.cleanDirectory(cspCacheDir);
-                    //ApiConfig.get().clearJarLoader();
+                if(cacheDir.exists()) FileUtils.cleanDirectory(cacheDir);
+                if(cspCacheDir.exists()) FileUtils.cleanDirectory(cspCacheDir);
+                if(cspCacheDir.exists()) FileUtils.cleanDirectory(jarCacheDir);
+                    // ApiConfig.get().clearJarLoader();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }).start();
         App.showToastShort(getContext(), "缓存已清空！");
         return;
