a01c2e05a2a2bd3f54094ed18c69252f5f4a0d75
jiqimaojiqimao <lilijuan_1980@163.com>
2026-08-17 14:31:15 +0800

    Update LrcView.java

--- app/src/main/java/com/github/tvbox/osc/subtitle/LrcView.java
+++ app/src/main/java/com/github/tvbox/osc/subtitle/LrcView.java
@@ -34,6 +34,7 @@ import java.lang.Math;
  * 新增：初始位不显示滚动动画
  * 新增：不是相邻行不不显示滚动动画
  * 新增：播放进度到当前行的上行或多行不显示滚动动画
+ * 20260817 新增：高亮进度平滑滤波 解决最版exoplayer对mp3的升级后字幕卡顿问题
  */
 public class LrcView extends View {
 
