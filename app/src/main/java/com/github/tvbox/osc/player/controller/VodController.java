3721225bede4485db79442a55e79cd1460d9cd22
jiqimaojiqimao <lilijuan_1980@163.com>
2026-08-23 18:00:36 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -420,15 +420,10 @@ public class VodController extends BaseController {
     private Runnable myRunnableXu = new Runnable() {
         @Override
         public void run() {
-            String width = Integer.toString(mControlWrapper.getVideoSize()[0]);
-            String height = Integer.toString(mControlWrapper.getVideoSize()[1]);
-            if(isInPlaybackState()) { //xuameng 重新选择解析视频大小不刷新
-                mVideoSize.setText("[ " + width + " X " + height + " ]");
-            }else{
-                mHandler.postDelayed(this, 100);
-                return;
-            }
             if(isInPlaybackState() && mControlWrapper.isPlaying()) { //xuameng音乐播放时图标判断
+                String width = Integer.toString(mControlWrapper.getVideoSize()[0]);
+                String height = Integer.toString(mControlWrapper.getVideoSize()[1]);
+                mVideoSize.setText("[ " + width + " X " + height + " ]");
                 if(!mIsDragging) {
                     mControlWrapper.startProgress(); //xuameng启动进程
                     mControlWrapper.startFadeOut();
