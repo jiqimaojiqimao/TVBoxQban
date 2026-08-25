0ad3989a9d561005ec91ddff5822c4255f62fdaf
jiqimaojiqimao <lilijuan_1980@163.com>
2026-08-25 21:56:54 +0800

    Update ExoMediaCodecSelector.java

--- app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaCodecSelector.java
+++ app/src/main/java/xyz/doikki/videoplayer/exo/ExoMediaCodecSelector.java
@@ -1,9 +1,9 @@
 package xyz.doikki.videoplayer.exo;
 
 import androidx.annotation.NonNull;
-import androidx.media3.mediacodec.MediaCodecSelector;
-import androidx.media3.mediacodec.MediaCodecUtil;
-import androidx.media3.mediacodec.MediaCodecInfo;
+import androidx.media3.exoplayer.mediacodec.MediaCodecSelector;
+import androidx.media3.exoplayer.mediacodec.MediaCodecUtil;
+import androidx.media3.exoplayer.mediacodec.MediaCodecInfo;
 
 import java.util.ArrayList;
 import java.util.List;
