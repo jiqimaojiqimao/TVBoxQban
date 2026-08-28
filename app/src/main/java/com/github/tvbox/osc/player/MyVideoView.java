bfc537f778ec7c9bf4b4834c11c7d2951cbf7919
jiqimaojiqimao <lilijuan_1980@163.com>
2026-08-28 11:48:51 +0800

    Update MyVideoView.java

--- app/src/main/java/com/github/tvbox/osc/player/MyVideoView.java
+++ app/src/main/java/com/github/tvbox/osc/player/MyVideoView.java
@@ -1,15 +1,10 @@
 package com.github.tvbox.osc.player;
 
 import android.content.Context;
-import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.Gravity;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
-import com.github.tvbox.osc.util.ImgUtilMusic;
 
 import master.flame.danmaku.controller.DrawHandler;
 import master.flame.danmaku.danmaku.model.BaseDanmaku;
@@ -20,7 +15,6 @@ import xyz.doikki.videoplayer.player.VideoView;
 
 public class MyVideoView extends VideoView implements DrawHandler.Callback {
     private DanmakuView danmuView;
-    private ImageView artworkView;
 
     public MyVideoView(@NonNull Context context) {
         super(context, null);
@@ -36,31 +30,6 @@ public class MyVideoView extends VideoView implements DrawHandler.Callback {
 
     public AbstractPlayer getMediaPlayer() {
         return mMediaPlayer;
-    }
-
-    public void setArtwork(String url) {
-        if (TextUtils.isEmpty(url)) {
-            clearArtwork();
-            return;
-        }
-        if (artworkView == null) {
-            artworkView = new ImageView(getContext());
-            artworkView.setBackgroundColor(android.graphics.Color.BLACK);
-            artworkView.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            artworkView.setClickable(false);
-            artworkView.setFocusable(false);
-            int index = mRenderView == null ? 0 : Math.min(1, mPlayerContainer.getChildCount());
-            mPlayerContainer.addView(artworkView, index, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER));
-        }
-        artworkView.setVisibility(VISIBLE);
-        ImgUtilMusic.load(url, artworkView, 0, 0, 0, "", ImageView.ScaleType.FIT_CENTER);
-    }
-
-    public void clearArtwork() {
-        if (artworkView != null) {
-            artworkView.setVisibility(GONE);
-            artworkView.setImageDrawable(null);
-        }
     }
 
     public int[] getVideoSize() {
