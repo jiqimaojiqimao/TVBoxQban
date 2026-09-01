ffb450be2a8b998e7ab0e6ecb7f92ebe6e49be9d
jiqimaojiqimao <lilijuan_1980@163.com>
2026-09-01 21:45:18 +0800

    Update DanmuLoadController.java

--- app/src/main/java/com/github/tvbox/osc/player/danmu/DanmuLoadController.java
+++ app/src/main/java/com/github/tvbox/osc/player/danmu/DanmuLoadController.java
@@ -62,8 +62,7 @@ public class DanmuLoadController {
             releaseView();
             if (controller != null) controller.setHasDanmu(!TextUtils.isEmpty(danmuText));
             return;
-        }
-        temporarilyClosed = false;  //xuameng 必须加 重置 临时弹幕状态判断
+        }    
         HashMap<Integer, Integer> maxLines = new HashMap<>();
         int maxLine = DanmuHelper.getMaxLine();
         maxLines.put(BaseDanmaku.TYPE_FIX_TOP, maxLine);
@@ -77,6 +76,7 @@ public class DanmuLoadController {
         danmakuContext.setDanmakuStyle(IDisplayer.DANMAKU_STYLE_STROKEN, 3)
                 .setDanmakuMargin(8);
         if (reload && !TextUtils.isEmpty(danmuText) && DanmuHelper.isOpen()) {
+            temporarilyClosed = false;  //xuameng 必须加 重置 临时弹幕状态判断 
             prepare(danmuText);
         }
     }
