948137e58a08b95d26e34359c9468a3ebcb154c5
jiqimaojiqimao <lilijuan_1980@163.com>
2026-09-02 17:25:29 +0800

    Update Subtitle.java

--- app/src/main/java/com/github/tvbox/osc/bean/Subtitle.java
+++ app/src/main/java/com/github/tvbox/osc/bean/Subtitle.java
@@ -1,59 +1,43 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.google.androidx.media3.exoplayer.extractor.ts;
+package com.github.tvbox.osc.bean;
 
-import androidx.media3.common.C;
-import androidx.media3.common.text.Cue;
-import androidx.media3.common.util.UnstableApi;
-import java.util.List;
+public class Subtitle {
 
-/** A subtitle consisting of timed {@link Cue}s. */
-@UnstableApi
-public interface Subtitle {
+    private String name;
 
-  /**
-   * Returns the index of the first event that occurs after a given time (exclusive).
-   *
-   * @param timeUs The time in microseconds.
-   * @return The index of the next event, or {@link C#INDEX_UNSET} if there are no events after the
-   *     specified time.
-   */
-  int getNextEventTimeIndex(long timeUs);
+    private String url;
 
-  /**
-   * Returns the number of event times, where events are defined as points in time at which the cues
-   * returned by {@link #getCues(long)} changes.
-   *
-   * @return The number of event times.
-   */
-  int getEventTimeCount();
+    private boolean isZip;
 
-  /**
-   * Returns the event time at a specified index.
-   *
-   * @param index The index of the event time to obtain.
-   * @return The event time in microseconds.
-   */
-  long getEventTime(int index);
+    public boolean getIsZip() {
+        return isZip;
+    }
 
-  /**
-   * Retrieve the cues that should be displayed at a given time.
-   *
-   * @param timeUs The time in microseconds.
-   * @return A list of cues that should be displayed, possibly empty.
-   */
-  List<Cue> getCues(long timeUs);
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setIsZip(boolean zip) {
+        isZip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Subtitle{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", isZip=" + isZip +
+                '}';
+    }
 }
