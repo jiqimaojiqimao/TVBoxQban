282ca15060b6370fd2f375d0c7cba28171b2fe45
jiqimaojiqimao <lilijuan_1980@163.com>
2026-09-16 16:17:10 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -81,6 +81,7 @@ public class HawkConfig {
     public static boolean isGetWp = false;  //xuameng下载壁纸
     public static boolean saveHistory = false;  //xuameng 存储历史记录
     public static boolean exoSubtitle = false;  //xuameng 判断当前是否播放EXO内置字幕
+    public static boolean refreshHomeIng = false;  //xuameng 判断当前是否正在切换主页源
 
     public static boolean hotVodDelete;
 }
